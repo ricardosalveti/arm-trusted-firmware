c11ab5af5a7fa38c57f18677f47d9963be73c313
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2018-09-25 18:44:58 +0530

    arm64: versal: Add support for new Xilinx Versal ACAPs
    Xilinx is introducing Versal, an adaptive compute acceleration platform
    (ACAP), built on 7nm FinFET process technology. Versal ACAPs combine Scalar
    Processing Engines, Adaptable Hardware Engines, and Intelligent Engines with
    leading-edge memory and interfacing technologies to deliver powerful
    heterogeneous acceleration for any application. The Versal AI Core series has
    five devices, offering 128 to 400 AI Engines. The series includes dual-core Arm
    Cortex-A72 application processors, dual-core Arm Cortex-R5 real-time
    processors, 256KB of on-chip memory with ECC, more than 1,900 DSP engines
    optimized for high-precision floating point with low latency.
    
    This patch adds Virtual QEMU platform support for
    this SoC "versal_virt".
    
    Signed-off-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

---
+++ plat/xilinx/versal/versal_private.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2014-2016, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __VERSAL_PRIVATE_H__
+#define __VERSAL_PRIVATE_H__
+
+void versal_config_setup(void);
+
+#endif /* __VERSAL_PRIVATE_H__ */

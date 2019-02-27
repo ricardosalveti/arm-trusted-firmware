26c8f3bf73de800d5e2c2215479c15a3b1eef640
Tejas Patel <tejas.patel@xilinx.com>
2019-02-27 18:44:54 +0530

    xilinx: versal: Add client wakeup API
    Implement client wakeup API for versal.
    
    Signed-off-by: Tejas Patel <tejas.patel@xilinx.com>
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>

--- plat/xilinx/versal/pm_service/pm_client.h
+++ plat/xilinx/versal/pm_service/pm_client.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Xilinx, Inc. All rights reserved.
+ * Copyright (c) 2018 - 2019, Xilinx, Inc. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -17,6 +17,7 @@
 
 /* Functions to be implemented by each PU */
 void pm_client_suspend(const struct pm_proc *proc, unsigned int state);
+void pm_client_wakeup(const struct pm_proc *proc);
 void pm_client_abort_suspend(void);
 
 /* Global variables to be set in pm_client.c */

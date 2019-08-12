d9c3a35d74dcb4fe107245a3b3d5265d3eb4b994
Ravi Patel <ravi.patel@xilinx.com>
2019-08-12 03:10:10 -0700

    plat: versal: Add InitFinalize API call
    Add support to call InitFinalize API in Versal which calls
    corresponding LibPM API.
    
    Signed-off-by: Ravi Patel <ravi.patel@xilinx.com>

--- plat/xilinx/versal/pm_service/pm_api_sys.h
+++ plat/xilinx/versal/pm_service/pm_api_sys.h
@@ -15,6 +15,7 @@
  **********************************************************/
 
 enum pm_ret_status pm_get_api_version(unsigned int *version);
+enum pm_ret_status pm_init_finalize(void);
 enum pm_ret_status pm_self_suspend(uint32_t nid,
 				   unsigned int latency,
 				   unsigned int state,
